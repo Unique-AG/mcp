3015ddd90e108b2a1166d0097855bb82b2ca0c9d
Konstantin Krauss <konsti@users.noreply.github.com>
2025-08-20 14:56:12 +0200

    feat(outlook): use prisma preview pg-adapter to remove need to install engines

--- servers/outlook-mcp/src/prisma/prisma.provider.ts
+++ servers/outlook-mcp/src/prisma/prisma.provider.ts
@@ -1,9 +1,17 @@
 import { PrismaClient } from '@generated/prisma';
 import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { AppConfig, AppSettings } from '../app-settings.enum';
 import { idPrefixExtension } from './id-prefix.extension';
 
 @Injectable()
 export class PrismaProvider extends PrismaClient {
+  public constructor(config: ConfigService<AppConfig, true>) {
+    const adapter = new PrismaPg({ connectionString: config.get(AppSettings.DATABASE_URL) });
+    super({ adapter });
+  }
+
   public withExtensions() {
     return this.$extends(idPrefixExtension);
   }
